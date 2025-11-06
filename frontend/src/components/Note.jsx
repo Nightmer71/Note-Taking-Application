6263fc7155b8da28b68945eb707f80914f5d47b8
Nightmer71 <totharon71@gmail.com>
2025-11-06 13:05:56 +0100

    Edited the Home.jsx and added Note.jsx, also added styling to those two pages

---
+++ frontend/src/components/Note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/Note.css";
+
+function Note({ note, onDelete }) {
+  const formattedDate = new Date(Note.created_at).toLocaleDateString("hu");
+
+  return (
+    <div className="note-container">
+      <p className="note-title">{note.title}</p>
+      <p className="note-content">{note.content}</p>
+      <p className="note-date">{formattedDate}</p>
+      <button className="delete-btn" onClick={() => onDelete(note.id)}>
+        Delete Note
+      </button>
+    </div>
+  );
+}
+
+export default Note;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,84 @@
+import { useState, useEffect } from "react";
+import api from "../api";
+import Note from "../components/Note";
+import "../styles/Home.css";
+
 function Home() {
-  return <div>Home</div>;
+  const [notes, setNotes] = useState([]);
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+
+  const getNotes = () => {
+    api
+      .get("/api/notes/")
+      .then((result) => result.data)
+      .then((data) => {
+        setNotes.data;
+        console.log(data);
+      })
+      .catch((error) => alert.error);
+  };
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  const deleteNote = (id) => {
+    api
+      .delete(`/api/notes/delete/${id}/`)
+      .then((result) => {
+        if (result.status === 204) alert("Note has been deleted!");
+        else alert("Failed to delete note!");
+        getNotes();
+      })
+      .catch((error) => alert(error));
+  };
+
+  const createNote = (e) => {
+    e.preventDefault();
+    api
+      .post("/api/notes", { content, title })
+      .then((result) => {
+        if (result.status === 201) alert("Note has been created!");
+        else "Failed to create note!";
+        getNotes();
+      })
+      .catch((error) => alert(error));
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>Notes</h2>
+        {notes.map((note) => {
+          <Note note={note} onDelete={deleteNote} key={note.id} />;
+        })}
+      </div>
+      <h2>Create a Note</h2>
+      <form onSubmit={createNote}>
+        <label htmlFor="title">Title:</label>
+        <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+
+        <label htmlFor="content">Content:</label>
+        <br />
+        <textarea
+          id="content"
+          name="content"
+          required
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 export default Home;
